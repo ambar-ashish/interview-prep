1493c0a589f99a2d286d1be0bda1721750414caa
Ambar Ashish <ambar.ashish10@gmail.com>
2020-08-06 15:49:39 +0530

    Heap Questions
    Top Heap Questions

---
+++ data-structures/leetcode/heap/KthLargestElementInArray.java
@@ -0,0 +1,27 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+//LC-215
+public class KthLargestElementInArray {
+
+    //Time Complexity -  O(NlogK)
+    //The time complexity of adding an element in a heap of size k is O(logk), and we do it N times that means
+    //O(Nlogk) time complexity for the algorithm
+    //Space Complexity - O(K) to store the heap elements
+    public int findKthLargest(int[] nums, int k) {
+
+        //Default order is ascending order
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int n: nums) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        return heap.poll();
+    }
+
+    //Quick Select solution should also be done for O(N) time complexity
+}
